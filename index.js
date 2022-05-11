70e5ceb0bc69460690c9966cdbbf9ac745bf5f36
Javier <frguinea.dwfc4@ucamp.utel.edu.mx>
2022-05-11 18:34:12 -0500

    Corrección de CORS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 
 let usuarios= [
@@ -11,6 +12,8 @@ let usuarios= [
 ];
 
 app.use(bodyParser.json());
+
+app.use(cors());
 
 app.use((req, res, next) =>{
     res.header('Access-Control-Allow-Origin', '*')
